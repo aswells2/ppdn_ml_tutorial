3213cbfb233b69829f64219770decab16518be12
Aaron S Wells <wellsas@mw.na.cat.com>
2017-06-22 08:59:30 -0500

    Signed-off-by: Aaron S Wells <wellsas@mw.na.cat.com>

--- starthere.py
+++ starthere.py
@@ -1,14 +1,21 @@
 import numpy as np
 import pandas as pd
 import quandl, math
-import numpy as np
 from sklearn import preprocessing #scaling is done on features. try to get values between -1 and 1
 from sklearn import cross_validation, svm
 from sklearn.linear_model import LinearRegression
+quandl.ApiConfig.api_key = '4EDgpr_zPmbrRoAXj6gQ'
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+
 
 df = quandl.get('WIKI/GOOGL')
 
-#print(df.head())
+#print(df.tail())
 
 ### Notice the double brackets when redefining the df.
 
@@ -32,10 +39,59 @@ forecast_out = int(math.ceil(0.01*len(df))) #math.ceil gets to ceiling which mea
 ### this equation allows us to change the number of days out we are predicting.
 ###In this case we are setting the number of days we are predicint out to 10% of
 ###the total number of data rows in our dataframe
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 ###our label will be the prediction of the label column (defined by forecast_col)
 ###predicted out some number of days (defined by forecast_out
+
+#print(df.head())
+X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X=X[:-forecast_out]
+
+
 df.dropna(inplace=True)
-print(df.head())
+y = np.array(df['label'])
+y= np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test,y_test)
+forecast_set = clf.predict(X_lately)
+#print(accuracy)
+
+#print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+'''
+clfsvm = svm.SVR()
+clfsvm.fit(X_train, y_train)
+accuracy1 = clfsvm.score(X_test, y_test)
+print(accuracy1)
+'''
+
+
+
+last_date = df.iloc[-1].name
+print(last_date)
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+#print(df.tail())
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
